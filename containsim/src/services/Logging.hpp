6a8c9ea899d02fcee51189302bc8fdd1d4a8a4e3
Dylan Ferris <myaka27too@gmail.com>
2023-12-21 11:23:11 -0500

    Integrated spdlog for logging

---
+++ containsim/src/services/Logging.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string_view>
+
+#include <spdlog/sinks/sink.h>
+#include <spdlog/logger.h>
+
+namespace services::logging {
+    using Sink = std::shared_ptr<spdlog::sinks::sink>;
+    using Logger = spdlog::logger;
+
+    Logger CreateLogger(std::string_view name);
+
+    void SetCommonSink(Sink sink);
+    Sink GetCommonSink();
+
+    Sink CreateConsoleSink();
+}
